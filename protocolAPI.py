3e4e9cacaffddbcbf4da6c77559a0a6718b1c085
tbags <shashankhegde@gmail.com>
2015-07-11 17:08:00 +0530

    Create protocolAPI.py

---
+++ protocolAPI.py
@@ -0,0 +1,12 @@
+"""
+This consists of all the wrapped functions and classes to facillitate easy protyping
+
+We recommend you to use/write any function you desire in/from this file only
+
+Lets RPi to the Greek
+
+"""
+import os
+import time
+from RPi import GPIO
+
