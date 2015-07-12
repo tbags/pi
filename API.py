e1abf2b769932a13868762503cf6053ac060f666
tbags <shashankhegde@gmail.com>
2015-07-12 16:02:30 +0530

    Update API.py

--- API.py
+++ API.py
@@ -3,10 +3,24 @@ This consists of all the wrapped functions and classes to facillitate easy proty
 
 We recommend you to use/write any function you desire in/from this file only
 
-Lets RPi to the Greek
+Lets bring RPi to the Greek
 
 """
 import os
 import time
+import socket
 from RPi import GPIO
 
+class CloudConnect(object):
+  """ this connects to the cloud intended for continuos to and fro communication
+  
+  Args: 
+  server: ip(string)
+  port: port(int)
+  
+  """
+  def __init__(self, ip, port):
+    self.ip = ip
+    self.port = port
+    
+  
